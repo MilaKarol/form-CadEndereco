bc5538e8612b199b7bd29879f09ee79293c69c39
Milena <milena.caroline@alunoescolassociais.org.br>
2024-09-20 08:10:44 -0300

    cad.js

---
+++ cad.js
@@ -0,0 +1,7 @@
+ 'use strict'; // Modo restrito // Este modo faz com que o JavaScript opere de forma mais segura e rigorosa, ajudando a evitar erros comuns de programação /*Consumo de API - https://viacep.com.br/*/ 
+
+
+ // Função para limpar formulario
+conts limparformulario = () =>{
+    
+}
