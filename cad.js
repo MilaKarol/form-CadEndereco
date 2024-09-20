201ec717de52a5a14fd7709905c81921766ecdec
Milena <milena.caroline@alunoescolassociais.org.br>
2024-09-20 08:17:03 -0300

    add cad.js

--- cad.js
+++ cad.js
@@ -3,5 +3,29 @@
 
  // Função para limpar formulario
 conts limparformulario = () =>{
-    
+    document.getElementById('logradouro').value = '';
+}
+// Função para limpar formulario do CEP
+conts limparformulario = () =>{
+    document.getElementById('CEP').value = '';
+}
+// Função para limpar formulario da localidade
+conts limparformulario = () =>{
+    document.getElementById('localidade').value = '';
+}
+// Função para limpar formulario do bairro
+conts limparformulario = () =>{
+    document.getElementById('Bairro').value = '';
+}
+// Função para limpar formulario do complemento
+conts limparformulario = () =>{
+    document.getElementById('Complemento').value = '';
+}
+// Função para limpar formulario da uf
+conts limparformulario = () =>{
+    document.getElementById('uf').value = '';
+}
+// Função para limpar formulario do número
+conts limparformulario = () =>{
+    document.getElementById('Número').value = '';
 }
